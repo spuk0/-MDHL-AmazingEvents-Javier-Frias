73b507621c52d1a49fc756f4acb675e792bb4105
spuk0 <jabfryyy@gmail.com>
2023-03-01 16:44:33 -0300

    Filtro de checkboxes mejorado en cuanto a modulacion

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 const searchFilter = document.getElementById("searchBar");
-
 searchFilter.addEventListener("submit", search);
 
 
@@ -17,30 +16,38 @@ function search(event){
             }
             return arrayCategories;
         })()
-    };
-    objEvent.events.forEach(event => {
-        dataFromSearch.Category.forEach(selectedCategory => {
-            if(event.category === selectedCategory){
-                cardText += `<div class="card cardMain" style="width: 15rem;">
-                                <img src="${event.image}" class="card-img-top eventImg" alt="${event.name}">
-                                <div class="card-body">
-                                    <h5 class="card-title">${event.name}</h5>
-                                    <p class="card-text">${event.date}</p>
-                                    <p class="card-text">${event.description}</p>
-                                    <div class="d-flex justify-content-between align-items-end">
-                                        <h6 class="cardPrice">$${event.price}</h6>
-                                        <a href="./pages/details.html" class="btn btn-primary btn-showMore">Show more</a>
-                                    </div>
-                                </div>
-                            </div>`
+    },
+    checkbox = {
+        ApplySearchCheckbox: ( () => {
+            if(dataFromSearch.Category.length == 0){
+                addCards();
+                alert("Please select a category or type a search");
+            }else {
+                objEvent.events.forEach(event => {
+                    dataFromSearch.Category.forEach(selectedCategory => {
+                        if(event.category === selectedCategory){
+                            cardText += `<div class="card cardMain" style="width: 15rem;">
+                                            <img src="${event.image}" class="card-img-top eventImg" alt="${event.name}">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${event.name}</h5>
+                                                <p class="card-text">${event.date}</p>
+                                                <p class="card-text">${event.description}</p>
+                                                <div class="d-flex justify-content-between align-items-end">
+                                                    <h6 class="cardPrice">$${event.price}</h6>
+                                                    <a href="./pages/details.html" class="btn btn-primary btn-showMore">Show more</a>
+                                                </div>
+                                            </div>
+                                        </div>`
+                        }
+                    })
+                });
+            
+                const cardsContainer = document.getElementById("cards");
+                cardsContainer.innerHTML = cardText;   
             }
-        })
-    });
-
-    const cardsContainer = document.getElementById("cards");
-    cardsContainer.innerHTML = cardText;
-    
-    
+        })()
+    };
+    checkbox.ApplySearchCheckbox;  
 }
 
 function createCards(){
