02c5be9fd3daa4485551721251cebee44227853e
spuk0 <jabfryyy@gmail.com>
2023-03-01 10:48:33 -0300

    Primeros pasos en filtro de busqueda

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,45 @@ const searchFilter = document.getElementById("searchBar");
 
 searchFilter.addEventListener("submit", search);
 
+
 function search(event){
+    let cardText = "";
     event.preventDefault();
     dataFromSearch = {
-        Category : event.target[1].value
+        Category : (() => {
+            let arrayCategories = [];
+            let categories = event.target.querySelectorAll("input[type=checkbox]");
+            for(let i=0; i < categories.length; i++){
+                if(categories[i].checked){
+                    arrayCategories.push(categories[i].value);
+                }
+            }
+            return arrayCategories;
+        })()
     };
-    console.log(dataFromSearch);
+    objEvent.events.forEach(event => {
+        dataFromSearch.Category.forEach(selectedCategory => {
+            if(event.category === selectedCategory){
+                cardText += `<div class="card cardMain" style="width: 15rem;">
+                                <img src="${event.image}" class="card-img-top eventImg" alt="${event.name}">
+                                <div class="card-body">
+                                    <h5 class="card-title">${event.name}</h5>
+                                    <p class="card-text">${event.date}</p>
+                                    <p class="card-text">${event.description}</p>
+                                    <div class="d-flex justify-content-between align-items-end">
+                                        <h6 class="cardPrice">$${event.price}</h6>
+                                        <a href="./pages/details.html" class="btn btn-primary btn-showMore">Show more</a>
+                                    </div>
+                                </div>
+                            </div>`
+            }
+        })
+    });
+
+    const cardsContainer = document.getElementById("cards");
+    cardsContainer.innerHTML = cardText;
+    
+    
 }
 
 function createCards(){
