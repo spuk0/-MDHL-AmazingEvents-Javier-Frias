facb36c9fc3bffc5c992bce5d99694877cd49f18
spuk0 <jabfryyy@gmail.com>
2023-03-02 11:07:59 -0300

    searchBar funciona

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,39 @@
+const cardsContainer = document.getElementById("cards");
 const checkboxesFilter = document.getElementById("searchBar");
 const searchWriteSpace = document.getElementById("search-write-space");
-searchWriteSpace.addEventListener("keyup",search);
+searchWriteSpace.addEventListener("keyup",searchText);
 checkboxesFilter.addEventListener("submit", search);
+
+
+function searchText(event){
+    let cardText = "";
+    let writedText = event.target.value.toLowerCase().trim();
+    objEvent.events.forEach(event => {
+        let nameEvent = event.name.toLowerCase();
+        if(nameEvent.includes(writedText)){
+            cardText += `<div class="card cardMain" style="width: 15rem;">
+                            <img src="${event.image}" class="card-img-top eventImg" alt="${event.name}">
+                            <div class="card-body">
+                                <h5 class="card-title">${event.name}</h5>
+                                <p class="card-text">${event.date}</p>
+                                <p class="card-text">${event.description}</p>
+                                <div class="d-flex justify-content-between align-items-end">
+                                    <h6 class="cardPrice">$${event.price}</h6>
+                                    <a href="./pages/details.html" class="btn btn-primary btn-showMore">Show more</a>
+                                </div>
+                            </div>
+                        </div>`
+        }
+    });
+    cardsContainer.innerHTML = cardText;
+}
+
 
 function search(event){
     let cardText = "";
+
     event.preventDefault();
+
     dataFromSearch = {
         category : (() => {
             let arrayCategories = [];
@@ -42,13 +70,10 @@ function search(event){
                         }
                     })
                 });
-            
-                const cardsContainer = document.getElementById("cards");
                 cardsContainer.innerHTML = cardText;   
             }
         })()
     };
-    checkbox.applySearchCheckbox;
 }
 
 function createCards(){
@@ -71,7 +96,6 @@ function createCards(){
 }
 
 function addCards(){
-    const cardsContainer = document.getElementById("cards");
     cardsContainer.innerHTML = createCards();
 }
 
