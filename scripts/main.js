9b07501fc69cec78c0ae33137f3fbf7c71145fdc
spuk0 <jabfryyy@gmail.com>
2023-03-02 13:12:00 -0300

    documentacion y legibilidad

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,53 @@
-/*Constantes y asignaciones de etiquetas*/
+/*=================Constantes-variables=================*/
 const cardsContainer = document.getElementById("cards");
 const checkboxesFilter = document.getElementById("searchBar");
 const searchWriteSpace = document.getElementById("search-write-space");
-searchWriteSpace.addEventListener("keyup",searchText); //Input text.
-checkboxesFilter.addEventListener("submit", search); //Boton submit.
+const bodyPage = document.querySelector("body");
+let htmlCard = ""; //Un string que almacenara el codigo html necesario para crear una carta.
 
+/*=================Eventos con referencias a funciones=================*/
+bodyPage.addEventListener("load", allCards()); //Al cargar la pagina genera las cartas.
+searchWriteSpace.addEventListener("keyup", searchText); //Al presionar teclas dentro del input ejecuta la funcion.
+checkboxesFilter.addEventListener("submit", search); //Al presionar el submit ejecuta la funcion.
+
+/*=================Funciones asignadas a eventos=================*/
+
+/*--Es para agregar cards al contenedor de cards
+        @param htmlCode - representa el codigo html que debe ir como parametro.
+--*/
+function addCards(htmlCode){
+    cardsContainer.innerHTML = htmlCode;
+}
+
+/*--Carga todas las cartas y las agrega al contenedor de cartas--*/
+function allCards(){
+    for (const event of objEvent.events ){
+        htmlCard += `<div class="card cardMain" style="width: 15rem;">
+                        <img src="${event.image}" class="card-img-top eventImg" alt="${event.name}">
+                            <div class="card-body">
+                                <h5 class="card-title">${event.name}</h5>
+                                <p class="card-text">${event.date}</p>
+                                <p class="card-text">${event.description}</p>
+                                <div class="d-flex justify-content-between align-items-end">
+                                    <h6 class="cardPrice">$${event.price}</h6>
+                                    <a href="./pages/details.html" class="btn btn-primary btn-showMore">Show more</a>
+                                </div>
+                            </div>
+                    </div>`
+    }
+    addCards(htmlCard);
+}
+
+/*--Realiza una busqueda del texto insertado en el input
+    @param event - representa el evento al cual esta funcion esta siendo referenciada.
+--*/
 function searchText(event){
-    let cardText = "";
-    let writedText = event.target.value.toLowerCase().trim();
-    objEvent.events.forEach(event => {
-        let nameEvent = event.name.toLowerCase();
-        if(nameEvent.startsWith(writedText)){
-            cardText += `<div class="card cardMain" style="width: 15rem;">
+    htmlCard = ""; //Limpia el html de las cartas del contenedor de cartas.
+    let writedText = event.target.value.toLowerCase().trim(); //Guarda en la variable lo que se este insertando en el input, y aplica metodos string.
+    objEvent.events.forEach(event => { 
+        let nameEvent = event.name.toLowerCase(); //Guarda el nombre del evento de la base de datos.
+        if(nameEvent.startsWith(writedText)){ //Si el evento coincide con lo escrito en el input, genera el html para su correspondiente carta.
+            htmlCard += `<div class="card cardMain" style="width: 15rem;">
                             <img src="${event.image}" class="card-img-top eventImg" alt="${event.name}">
                             <div class="card-body">
                                 <h5 class="card-title">${event.name}</h5>
@@ -25,15 +61,12 @@ function searchText(event){
                         </div>`
         }
     });
-    cardsContainer.innerHTML = cardText;
+    addCards(htmlCard); //Todas las cartas que pudieron generarse seran agregadas al contenedor de cartas.
 }
 
 
 function search(event){
-    let cardText = "";
-
     event.preventDefault();
-
     dataFromSearch = {
         category : (() => {
             let arrayCategories = [];
@@ -55,7 +88,7 @@ function search(event){
                 objEvent.events.forEach(event => {
                     dataFromSearch.category.forEach(selectedCategory => {
                         if(event.category === selectedCategory){
-                            cardText += `<div class="card cardMain" style="width: 15rem;">
+                            htmlCard += `<div class="card cardMain" style="width: 15rem;">
                                             <img src="${event.image}" class="card-img-top eventImg" alt="${event.name}">
                                             <div class="card-body">
                                                 <h5 class="card-title">${event.name}</h5>
@@ -70,36 +103,11 @@ function search(event){
                         }
                     })
                 });
-                cardsContainer.innerHTML = cardText;  
+                addCards(htmlCard); 
             }
         })()
     };
 }
-
-function createCards(){
-    let cardText = "";
-    for (const event of objEvent.events ){
-        cardText += `<div class="card cardMain" style="width: 15rem;">
-                        <img src="${event.image}" class="card-img-top eventImg" alt="${event.name}">
-                            <div class="card-body">
-                                <h5 class="card-title">${event.name}</h5>
-                                <p class="card-text">${event.date}</p>
-                                <p class="card-text">${event.description}</p>
-                                <div class="d-flex justify-content-between align-items-end">
-                                    <h6 class="cardPrice">$${event.price}</h6>
-                                    <a href="./pages/details.html" class="btn btn-primary btn-showMore">Show more</a>
-                                </div>
-                            </div>
-                    </div>`
-    }
-    return cardText;
-}
-
-function addCards(){
-    cardsContainer.innerHTML = createCards();
-}
-
-addCards();
 
 
 
