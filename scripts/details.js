b6b914b9d6b626b33d9261751e3ae3e2f4b7cd81
spuk0 <jabfryyy@gmail.com>
2023-03-08 11:27:17 -0300

    details casi completo, falta ver el tamaño de la card o detalles

--- scripts/details.js
+++ scripts/details.js
@@ -6,7 +6,7 @@ const theEvent = objEvent.events.find(ev => ev._id == id);
 
 const div = document.querySelector('#cardsDetails');
 div.innerHTML=`<div class="card mb-3 col-12 cardDetails align-items-center">
-                    <div class="row">
+                 <div class="row">
                     <div class="col-md-4 align-self-center">
                         <img
                         src="${theEvent.image}"
@@ -15,19 +15,47 @@ div.innerHTML=`<div class="card mb-3 col-12 cardDetails align-items-center">
                         />
                     </div>
                     <div class="col-md-8">
-                        <div class="card-body">
+                     <div class="card-body">
                             <h2 class="card-title">${theEvent.name}</h2>
-                            <p class="card-text">${theEvent.date}</p>
-                            <h5>Description: </h5>
+                            <p class="card-text card-date">${theEvent.date}</p>
+                            <h5 class="card-title">Description: </h5>
                             <p class="card-text">${theEvent.description}</p>
-                            <p class="card-text">Assistance: ${theEvent.assistance}</p>
-                            <p class="card-text">Capacity: ${theEvent.capacity}</p>
-                            <p class="card-text">Category: ${theEvent.category}</p>
-                            <p class="card-text">Place: ${theEvent.place}</p>
-                            <p class="card-text">Price: ${theEvent.price}</p>
-                        </div>
+                         <div class="card-details-text d-flex flex-column">
+                            <div class="card-text-details d-flex align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--colorLogo)" class="bi bi-card-list" viewBox="0 0 16 16">
+                                    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+                                    <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+                                </svg>
+                                <p class="card-text">Assistance: ${theEvent.assistance}</p>
+                            </div>
+                            <div class="card-text-details d-flex align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--colorLogo)" class="bi bi-people" viewBox="0 0 16 16">
+                                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
+                                </svg>
+                                <p class="card-text">Capacity: ${theEvent.capacity}</p>
+                            </div>
+                            <div class="card-text-details d-flex align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--colorLogo)" class="bi bi-stickies" viewBox="0 0 16 16">
+                                    <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5V13a1 1 0 0 0 1 1V1.5a.5.5 0 0 1 .5-.5H14a1 1 0 0 0-1-1H1.5z"/>
+                                    <path d="M3.5 2A1.5 1.5 0 0 0 2 3.5v11A1.5 1.5 0 0 0 3.5 16h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 16 9.586V3.5A1.5 1.5 0 0 0 14.5 2h-11zM3 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V9h-4.5A1.5 1.5 0 0 0 9 10.5V15H3.5a.5.5 0 0 1-.5-.5v-11zm7 11.293V10.5a.5.5 0 0 1 .5-.5h4.293L10 14.793z"/>
+                                </svg>
+                                <p class="card-text">Category: ${theEvent.category}</p>
+                            </div>
+                            <div class="card-text-details d-flex align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--colorLogo)" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                </svg>
+                                <p class="card-text">Place: ${theEvent.place}</p>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between">
+                                <a href="../index.html" class="btn btn-primary btn-custom">Back</a>
+                                <p class="card-text card-price">Price: ${theEvent.price}</p>
+                            </div>
+                         </div>
+                     </div>
                     </div>
-                    </div>
+                 </div>
                 </div>`
 
 
